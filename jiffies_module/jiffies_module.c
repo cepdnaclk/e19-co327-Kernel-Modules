43d4f8af5e6c9f6153118c65cc7a64863ee902fe
e19166 <e19166@eng.pdn.ac.lk>
2024-05-11 13:58:37 +0530

    wrote a read function for proc/jiffies

--- jiffies_module/jiffies_module.c
+++ jiffies_module/jiffies_module.c
@@ -1,3 +1,16 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#
+#include <linux/module.h>  // for documentation and macros
+#include <linux/kernel.h>  // for kernel macros and functions, kernel data types, kernel version info
+#include <linux/proc_fs.h>  // for proc file system (procfs) interface, creating procs entries, reading and writing to procfs
+#include <asm/param.h>  // for HZ (jiffies frequency>
+
+MODULE_LICENSE("GPL ");
+MODULE_AUTHOR("Wishula Jayathunga");
+
+static struct proc_dir_entry *jiffies_entry;
+
+// Read operation for /proc/jiffies
+static ssize_t jiffies_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
+  {
+    char jiffies_str[20];
+    snprintf(jiffies_str, sizeof
+  } 
